730e91c0f8038272d3758f44906862b35a313548
ArdhenduSeSahoo <ardhendu@esspl.com>
2025-02-13 11:13:35 +0530

    Add project files.

---
+++ NORA.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NORA.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ NORA/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NORA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
